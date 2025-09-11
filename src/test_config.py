6e722b887218a452c8eeff207d5ef5972b63262e
zheddhe <remy.canal@live.fr>
2025-09-11 12:25:37 +0200

    Ajustement après démo et changement emplacement test_config

--- src/test_config.py
+++ src/test_config.py
@@ -10,15 +10,15 @@ SITE_TEST = {
     #     "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
     #     "iter_grid_search": 0,
     # },
-    # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-    #     "sub_dir": "Sebastopol_N-S_v2",
-    #     "interim_file_name": "daily_1.csv",
-    #     "processed_file_name": "daily_1_with_feats.csv",
-    #     "range": (0.5, 75.5),  # a portion of the original range TODO : use exact timestamp ?
-    #     "temp_feats": [7, 1, 24],
-    #     "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
-    #     "iter_grid_search": 0,
-    # }
+    ('Totem 73 boulevard de Sébastopol', 'N-S'): {
+        "sub_dir": "Sebastopol_N-S_v2",
+        "interim_file_name": "daily_1.csv",
+        "processed_file_name": "daily_1_with_feats.csv",
+        "range": (0.5, 75.5),  # a portion of the original range TODO : use exact timestamp ?
+        "temp_feats": [7, 1, 24],
+        "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
+        "iter_grid_search": 0,
+    }
     # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
     #     "sub_dir": "Sebastopol_N-S_v2",
     #     "interim_file_name": "daily_2.csv",
@@ -28,13 +28,13 @@ SITE_TEST = {
     #     "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
     #     "iter_grid_search": 0,
     # }
-    ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-        "sub_dir": "Sebastopol_N-S_v2",
-        "interim_file_name": "daily_3.csv",
-        "processed_file_name": "daily_3_with_feats.csv",
-        "range": (1.5, 76.5),  # a portion of the original range TODO : use exact timestamp ?
-        "temp_feats": [7, 1, 24],
-        "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
-        "iter_grid_search": 0,
-    }
+    # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
+    #     "sub_dir": "Sebastopol_N-S_v2",
+    #     "interim_file_name": "daily_3.csv",
+    #     "processed_file_name": "daily_3_with_feats.csv",
+    #     "range": (1.5, 76.5),  # a portion of the original range TODO : use exact timestamp ?
+    #     "temp_feats": [7, 1, 24],
+    #     "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
+    #     "iter_grid_search": 0,
+    # }
 }
