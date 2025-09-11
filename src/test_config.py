bba18368066b54ce2a09d67af180f7a63060d21c
zheddhe <remy.canal@live.fr>
2025-09-11 17:08:11 +0200

    ajustement test_config au niveau src

--- src/test_config.py
+++ src/test_config.py
@@ -1,24 +1,24 @@
 RAW_FILE_NAME = "comptage-velo-donnees-compteurs-2024-2025_Enriched_ML-ready_data.csv"
 
 SITE_TEST = {
-    # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-    #     "sub_dir": "Sebastopol_N-S_v2",
-    #     "interim_file_name": "initial.csv",
-    #     "processed_file_name": "initial_with_feats.csv",
-    #     "range": (0.0, 75.0),  # a portion of the original range TODO : use exact timestamp ?
-    #     "temp_feats": [7, 1, 24],
-    #     "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
-    #     "iter_grid_search": 0,
-    # },
     ('Totem 73 boulevard de Sébastopol', 'N-S'): {
         "sub_dir": "Sebastopol_N-S_v2",
-        "interim_file_name": "daily_1.csv",
-        "processed_file_name": "daily_1_with_feats.csv",
-        "range": (0.5, 75.5),  # a portion of the original range TODO : use exact timestamp ?
+        "interim_file_name": "initial.csv",
+        "processed_file_name": "initial_with_feats.csv",
+        "range": (0.0, 75.0),  # a portion of the original range TODO : use exact timestamp ?
         "temp_feats": [7, 1, 24],
         "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
         "iter_grid_search": 0,
-    }
+    },
+    # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
+    #     "sub_dir": "Sebastopol_N-S_v2",
+    #     "interim_file_name": "daily_1.csv",
+    #     "processed_file_name": "daily_1_with_feats.csv",
+    #     "range": (0.5, 75.5),  # a portion of the original range TODO : use exact timestamp ?
+    #     "temp_feats": [7, 1, 24],
+    #     "test_ratio": 0.25,  # portion train/test TODO : use limit timestamp ?
+    #     "iter_grid_search": 0,
+    # }
     # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
     #     "sub_dir": "Sebastopol_N-S_v2",
     #     "interim_file_name": "daily_2.csv",
