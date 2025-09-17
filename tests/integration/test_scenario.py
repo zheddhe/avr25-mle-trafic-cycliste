1c70e6f762b0323994e88d3763d17f4bfb8f9d8a
zheddhe <remy.canal@live.fr>
2025-09-17 13:57:39 +0200

    ajustement archivage DVC (séparation _dvcrepro / _mlops / _integration selon le mode de lancement pour séparer les archivage / expériences)

--- tests/integration/test_scenario.py
+++ tests/integration/test_scenario.py
@@ -2,7 +2,7 @@ RAW_FILE_NAME = "comptage-velo-donnees-compteurs-2024-2025_Enriched_ML-ready_dat
 
 SITE_TEST = {
     ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-        "sub_dir": "Sebastopol_N-S_v2",
+        "sub_dir": "Sebastopol_N-S_integration",
         "interim_file_name": "initial.csv",
         "processed_file_name": "initial_with_feats.csv",
         "range": (0.0, 75.0),  # a portion of the original range TODO : use exact timestamp ?
@@ -11,7 +11,7 @@ SITE_TEST = {
         "iter_grid_search": 0,
     },
     # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-    #     "sub_dir": "Sebastopol_N-S_v2",
+    #     "sub_dir": "Sebastopol_N-S_integration",
     #     "interim_file_name": "daily_1.csv",
     #     "processed_file_name": "daily_1_with_feats.csv",
     #     "range": (0.5, 75.5),  # a portion of the original range TODO : use exact timestamp ?
@@ -20,7 +20,7 @@ SITE_TEST = {
     #     "iter_grid_search": 0,
     # }
     # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-    #     "sub_dir": "Sebastopol_N-S_v2",
+    #     "sub_dir": "Sebastopol_N-S_integration",
     #     "interim_file_name": "daily_2.csv",
     #     "processed_file_name": "daily_2_with_feats.csv",
     #     "range": (1, 76),  # a portion of the original range TODO : use exact timestamp ?
@@ -29,7 +29,7 @@ SITE_TEST = {
     #     "iter_grid_search": 0,
     # }
     # ('Totem 73 boulevard de Sébastopol', 'N-S'): {
-    #     "sub_dir": "Sebastopol_N-S_v2",
+    #     "sub_dir": "Sebastopol_N-S_integration",
     #     "interim_file_name": "daily_3.csv",
     #     "processed_file_name": "daily_3_with_feats.csv",
     #     "range": (1.5, 76.5),  # a portion of the original range TODO : use exact timestamp ?
