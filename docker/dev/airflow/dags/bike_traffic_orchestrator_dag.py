b67bc95d0d9541cc7c2d00f891a18618824ba2ec
Rémy Canal <remy.canal@live.fr>
2026-06-11 16:16:25 +0200

    Align dev Airflow DAG config with service runtime

--- docker/dev/airflow/dags/bike_traffic_orchestrator_dag.py
+++ docker/dev/airflow/dags/bike_traffic_orchestrator_dag.py
@@ -1,49 +1,45 @@
-# src/airflow/dags/bike_traffic_orchestrator_dag.py
+"""Development orchestrator DAG for bike traffic pipelines."""
+
 from __future__ import annotations
 
 import logging
 from datetime import datetime
 
-from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
-from common.utils import _list_counters_payload
+from airflow.sdk import DAG
+
+from common.utils import _list_counters_payload, _load_concurrency_config
 
 logger = logging.getLogger("airflow.task")
+logger.info("[orchestrator] Development DAG loaded")
 
-# --------------------------------------------------------------------------- #
-# Orchestrator DAG
-# --------------------------------------------------------------------------- #
+concurrency = _load_concurrency_config()
+
+
 with DAG(
     dag_id="bike_traffic_orchestrator",
     description="Parent DAG orchestrating dev init and daily DAGs for counters.",
-    start_date=datetime(2025, 9, 20),
-    schedule="@daily",
+    start_date=datetime(2025, 9, 1),
+    schedule=None,
     catchup=False,
-    max_active_runs=1,
+    max_active_runs=concurrency.orchestrator_max_active_runs,
+    max_active_tasks=concurrency.orchestrator_max_active_tasks,
     tags=["ops", "orchestration", "bike", "dev"],
 ) as dag:
-    logger.info("[orchestrator] DAG loaded")
-
-    get_counters = PythonOperator(
-        task_id="get_counters",
-        python_callable=_list_counters_payload,
-    )
+    counters = _list_counters_payload()
 
     run_init = TriggerDagRunOperator.partial(
         task_id="run_init",
         trigger_dag_id="bike_traffic_init",
-        poke_interval=5,
         wait_for_completion=True,
-        pool="sequential_counters",
-    ).expand(conf=get_counters.output)
+        reset_dag_run=True,
+    ).expand(conf=counters)
 
     run_daily = TriggerDagRunOperator.partial(
         task_id="run_daily",
         trigger_dag_id="bike_traffic_daily",
-        poke_interval=5,
         wait_for_completion=True,
-        pool="sequential_counters",
-    ).expand(conf=get_counters.output)
+        reset_dag_run=True,
+    ).expand(conf=counters)
 
-    get_counters >> run_init >> run_daily  # type: ignore
+    run_init >> run_daily  # type: ignore
